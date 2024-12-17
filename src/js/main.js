123a1a4b10d406f6690a4e633292da5c1336558b
Julio Viche <jvichecastillo@gmail.com>
2024-12-17 08:47:48 -0500

    Create download button, Change script
    - class SkinFace
    - const SKIN_HEAD
    - function createSkin
    - validate px
    - downloadButton event listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,73 @@
-const skinInput = document.getElementById('skinInput')
-const processButton = document.getElementById('processButton')
+const skinInput = document.getElementById('skinInput');
+const downloadButton = document.getElementById('downloadButton');
 
-let skinFile = null
+let skinFile = null; // original file 64px 64px
+let miniSkinDataURL = null; // result as data URL
+
+class SkinFace {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+}
+
+const SKIN_HEAD =  {
+    up:    new SkinFace(8, 0, 8, 8),
+    down: new SkinFace(16, 0, 8, 8),
+    right: new SkinFace(0, 8, 8, 8),
+    front: new SkinFace(8, 8, 8, 8),
+    left: new SkinFace(16, 8, 8, 8),
+    back: new SkinFace(24, 8, 8, 8),
+}
+
+function createSkin(skinImg) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    canvas.width = 32;
+    canvas.height = 32;
+
+    ctx.drawImage(skinImg, SKIN_HEAD.front.x, SKIN_HEAD.front.y, SKIN_HEAD.front.w, SKIN_HEAD.front.h, 0, 0, SKIN_HEAD.front.w, SKIN_HEAD.front.h);
+
+    return canvas.toDataURL('image/png');
+}
 
 skinInput.addEventListener('change', (event) => {
-    const file = event.target.files[0]
+    const file = event.target.files[0];
 
-    if (file && file.type === 'image/png') {
-        skinFile = file
-        processButton.disabled = false
-        alert('Imagen cargada correctamente.')
-    } else {
-        alert('Por favor selecciona un archivo PNG válido.')
-        skinFile = null
+    if (!file || file.type !== 'image/png') {
+        alert('Por favor selecciona un archivo PNG válido.');
+        return;
     }
-});
 
-processButton.addEventListener('click', () => {
-    if (!skinFile) return
+    skinFile = file;
 
-    const reader = new FileReader()
+    const reader = new FileReader();
 
     reader.onload = (event) => {
-        const arrayBuffer = event.target.result;
+        const skinImg = new Image();
 
-        console.log('Tamaño del archivo: ' + (arrayBuffer.byteLength / 1024) + ' kb');
+        skinImg.src = event.target.result;
+        skinImg.onload = () => {
+            if (skinImg.width != 64 || skinImg.height != 64) {
+                alert('Por favor selecciona un archivo de 64x64 px. [' + skinImg.width + 'x' + skinImg.height + ']');
+                return;
+            }
+
+            miniSkinDataURL = createSkin(skinImg);
+        }
+
+        downloadButton.disabled = false;
     };
+    reader.readAsDataURL(skinFile);  
+});
 
-    reader.readAsArrayBuffer(skinFile);
+downloadButton.addEventListener('click', () => {
+    const link = document.createElement('a');
+
+    link.href = miniSkinDataURL;
+    link.download = 'test.png';
+    link.click();
 });
