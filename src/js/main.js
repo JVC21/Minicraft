f4df103515302576e26a061e0106b88296ed5430
Julio Viche <jvichecastillo@gmail.com>
2024-12-18 09:25:56 -0500

    Update main.js
    - Draw functions

--- src/js/main.js
+++ src/js/main.js
@@ -11,27 +11,137 @@ class SkinFace {
         this.w = w;
         this.h = h;
     }
+
+    draw(ctx, skinImg, dx, dy, dh, dw) {
+        ctx.drawImage(skinImg, this.x, this.y, this.w, this.h, dx, dy, dh, dw);
+    }
 }
 
-const SKIN_HEAD =  {
-    up:    new SkinFace(8, 0, 8, 8),
-    down: new SkinFace(16, 0, 8, 8),
-    right: new SkinFace(0, 8, 8, 8),
-    front: new SkinFace(8, 8, 8, 8),
-    left: new SkinFace(16, 8, 8, 8),
-    back: new SkinFace(24, 8, 8, 8),
+class SkinHead {
+    constructor(x, y) { 
+        this.up    = new SkinFace(x + 8 , y + 0, 8, 8);
+        this.down  = new SkinFace(x + 16, y + 0, 8, 8);
+        this.right = new SkinFace(x + 0 , y + 8, 8, 8);
+        this.front = new SkinFace(x + 8 , y + 8, 8, 8);
+        this.left  = new SkinFace(x + 16, y + 8, 8, 8);
+        this.back  = new SkinFace(x + 24, y + 8, 8, 8);
+    }
 }
 
-function createSkin(skinImg) {
+class SkinBody {
+    constructor(x, y) { 
+        this.up    = new SkinFace(x + 4 , y + 0, 8, 4 );
+        this.down  = new SkinFace(x + 12, y + 0, 8, 4 );
+        this.right = new SkinFace(x + 0 , y + 4, 4, 12);
+        this.front = new SkinFace(x + 4 , y + 4, 8, 12);
+        this.left  = new SkinFace(x + 12, y + 4, 4, 12);
+        this.back  = new SkinFace(x + 16, y + 4, 8, 12);
+    }
+}
+
+class SkinLimb {
+    constructor(x, y) {
+        this.up    = new SkinFace(x + 4 , y + 0, 8, 4 );
+        this.down  = new SkinFace(x + 8, y + 0, 8, 4 );
+        this.right = new SkinFace(x + 0 , y + 4, 4, 12);
+        this.front = new SkinFace(x + 4 , y + 4, 8, 12);
+        this.left  = new SkinFace(x + 8, y + 4, 4, 12);
+        this.back  = new SkinFace(x + 12, y + 4, 8, 12);
+    }
+}
+
+const HEAD_1 = new SkinHead(0 , 0 );
+const HEAD_2 = new SkinHead(32, 0 );
+const BODY_1 = new SkinBody(16, 16);
+const BODY_2 = new SkinBody(16, 32);
+const ARMR_1 = new SkinBody(40, 16);
+const ARMR_2 = new SkinBody(40, 32);
+const ARML_1 = new SkinBody(32, 48);
+const ARML_2 = new SkinBody(48, 48);
+const LEGR_1 = new SkinBody(0 , 16);
+const LEGR_2 = new SkinBody(0 , 32);
+const LEGL_1 = new SkinBody(16, 48);
+const LEGL_2 = new SkinBody(0 , 48);
+
+function scale(canvas, scale) {
+    let scaledCanvas = document.createElement('canvas');
+    let scaledCtx = scaledCanvas.getContext('2d');
+
+    scaledCanvas.width = canvas.width * scale;
+    scaledCanvas.height = canvas.height * scale;
+
+    scaledCtx.imageSmoothingEnabled = false;
+    scaledCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, scaledCanvas.width, scaledCanvas.height);
+
+    return scaledCanvas;
+}
+
+function drawSkinHead(skinImg) {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
     
-    canvas.width = 32;
-    canvas.height = 32;
+    canvas.width = 12;
+    canvas.height = 8;
 
-    ctx.drawImage(skinImg, SKIN_HEAD.front.x, SKIN_HEAD.front.y, SKIN_HEAD.front.w, SKIN_HEAD.front.h, 0, 0, SKIN_HEAD.front.w, SKIN_HEAD.front.h);
+    HEAD_1.front.draw(ctx, skinImg, 4, 0, 8, 8);
+    HEAD_1.right.draw(ctx, skinImg, 0, 0, 4, 8);
+    HEAD_2.front.draw(ctx, skinImg, 4, 0, 8, 8);
+    HEAD_2.right.draw(ctx, skinImg, 0, 0, 4, 8);
+    ctx.fillStyle = '#00000055';
+    ctx.fillRect(0, 0, 4, 8);
 
-    return canvas.toDataURL('image/png');
+    return canvas;
+}
+
+function drawSkinBody(skinImg) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    canvas.width = 6;
+    canvas.height = 4;
+
+    BODY_1.front.draw(ctx, skinImg, 2, 0, 4, 4);
+    BODY_1.right.draw(ctx, skinImg, 0, 0, 2, 4);
+    BODY_2.front.draw(ctx, skinImg, 2, 0, 4, 4);
+    BODY_2.right.draw(ctx, skinImg, 0, 0, 2, 4);
+    ctx.fillStyle = '#00000055';
+    ctx.fillRect(0, 0, 2, 4);
+
+    return canvas;
+}
+
+function drawSkinLimb(skinImg, limb1, limb2) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    canvas.width = 4;
+    canvas.height = 4;
+
+    limb1.front.draw(ctx, skinImg, 2, 0, 2, 4);
+    limb1.right.draw(ctx, skinImg, 0, 0, 2, 4);
+    limb2.front.draw(ctx, skinImg, 2, 0, 2, 4);
+    limb2.right.draw(ctx, skinImg, 0, 0, 2, 4);
+    ctx.fillStyle = '#00000055';
+    ctx.fillRect(0, 0, 2, 4);
+
+    return canvas;
+}
+
+function drawSkin(skinImg) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    canvas.width = 16;
+    canvas.height = 16;
+
+    ctx.drawImage(drawSkinHead(skinImg), 2, 0, 12, 8);
+    ctx.drawImage(drawSkinLimb(skinImg, ARML_1, ARML_2), 9, 8, 4, 4);
+    ctx.drawImage(drawSkinBody(skinImg), 5, 8, 6, 4);
+    ctx.drawImage(drawSkinLimb(skinImg, ARMR_1, ARMR_2), 3, 8, 4, 4);
+    ctx.drawImage(drawSkinLimb(skinImg, LEGL_1, LEGL_2), 7, 12, 4, 4);
+    ctx.drawImage(drawSkinLimb(skinImg, LEGR_1, LEGR_2), 5, 12, 4, 4);
+
+    return scale(canvas, 10).toDataURL('image/png');
 }
 
 skinInput.addEventListener('change', (event) => {
@@ -56,7 +166,7 @@ skinInput.addEventListener('change', (event) => {
                 return;
             }
 
-            miniSkinDataURL = createSkin(skinImg);
+            miniSkinDataURL = drawSkin(skinImg);
         }
 
         downloadButton.disabled = false;
